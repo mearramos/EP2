e1ce8422d15cdebc04afcdaf70648c59e7d09e20
Maria Ramos <mariaramossilva901@gmail.com>
2024-10-22 18:50:31 -0300

    Exercício 2

--- funcoes.py
+++ funcoes.py
@@ -8,3 +8,13 @@ def define_posicoes(linha, coluna, orientacao, tamanho):
             posicoes.append([linha, coluna + i])
 
     return posicoes
+
+def preenche_frota(frota, nome, linha, coluna, orientacao, tamanho):
+    resultado_define = define_posicoes(linha, coluna, orientacao, tamanho)
+
+    if nome in frota:
+        frota[nome].append(resultado_define)
+    else:
+        frota[nome] = [resultado_define]
+    
+    return frota
